8429dfc71950f31a09aa1117241bd16851c1bd20
MashaGuzhva <mguzhvao.o@gmail.com>
2026-03-22 22:57:02 +0100

    try to create array in the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+struct Array {
+    int* data;
+    int size;
+
+    Array(int s) {
+        size = s;
+        data = new int[size];
+    }
+
+    ~Array() {
+        delete[] data;
+    }
+};
+
 int main() {
-    std::cout << "Program works\n";
+    Array arr(5);
+    arr.data[0] = 10;
+    arr.data[1] = 20;
+    arr.data[2] = 30;
+    arr.data[3] = 40;
+    arr.data[4] = 50;
+
+    for (int i = 0; i < arr.size; ++i) {
+        std::cout << arr.data[i] << " ";
+    }
+
+    std::cout << "\n";
     return 0;
 }
