00f1cf3476878c23630ad1792064ecae5ac581d7
MashaGuzhva <mguzhvao.o@gmail.com>
2026-04-11 21:39:05 +0200

    add shellHalfGaps sort

--- include/algorytmsSorting/ShellSort.h
+++ include/algorytmsSorting/ShellSort.h
@@ -5,14 +5,15 @@
 #ifndef PROJECT_SHELLSORT_H
 #define PROJECT_SHELLSORT_H
 #include "structures/Array.h"
+#include "Parameters.h"
 
 class ShellSort {
 public:
-    //static - metoda jest niezależna od obiektu
-    //nie ma potrzeby tworzenia obiektu QuickSort
+    //static - metoda jest niezależna od obiektu,
+    //nie ma potrzeby tworzenia obiektu,
     //funkcja została po prostu umieszczona w klasie
     // Array& - referencja na oryginalne tablice
-    static void sort(Array& array);
+    static void sort(Array& array,  Parameters::ShellParameters parameter);
 };
 
 
--- src/algorytmsSorting/ShellSort.cpp
+++ src/algorytmsSorting/ShellSort.cpp
@@ -3,12 +3,38 @@
 //
 #include "algorytmsSorting/ShellSort.h"
 
-void ShellSort::sort(Array& array)
-{
-    // jeśli tablica ma 0 lub 1 element, to nic sortowac
+
+static void insertionWithGap(Array& array, int gap) {
+    for (int i = gap; i < array.getSize(); ++i) {
+        const int temp = array.setId(i);
+        int j = i;
+
+        while (j >= gap && array.setId(j - gap) > temp) {
+            array.setId(j) = array.setId(j - gap);
+            j -= gap;
+        }
+
+        array.setId(j) = temp;
+    }
+}
+
+static void shellHalfGaps(Array& array) {
+    for (int gap = array.getSize() / 2; gap > 0; gap /= 2) {
+        insertionWithGap(array, gap);
+    }
+}
+
+
+void ShellSort::sort(Array& array, Parameters::ShellParameters parameter) {
     if (array.getSize() <= 1) {
         return;
     }
+
+    if (parameter == Parameters::ShellParameters::option2) {
+        return;
+    }
+
+    shellHalfGaps(array);
 
     // sortujemy tablicę od pierwszego do ostatniego indeksu
 }
