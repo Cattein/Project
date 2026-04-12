0f4957aa79cf221ba3bd11c8452e4c42877b16f9
MashaGuzhva <mguzhvao.o@gmail.com>
2026-04-12 22:27:51 +0200

    change random but question with seed is open

--- src/benchmark/RandomArrayGenerator.cpp
+++ src/benchmark/RandomArrayGenerator.cpp
@@ -1,65 +1,109 @@
-//
-// Created by MashaGuzhva on 12/04/2026.
-//
-
 #include "benchmark/RandomArrayGenerator.h"
 
 #include <new>
 #include <cstdlib>
-#include <ctime>
 
-// wypełnia tablicę losowymi liczbami
-bool RandomArrayGenerator::fillRandom(Array& array) {
-    // static sprawia, że ta zmienna nie tworzy się od nowa przy każdym wywołaniu
-    static bool seeded = false;    // seeded zapamiętuje, czy generator został już ustawiony
+// stały seed
+static void seedRandomOnce() {
+    // static sprawia, że generator ustawiamy tylko raz
+    static bool seeded = false;
 
-
-    // jeśli generator jeszcze nie był ustawiony, ustawiamy go aktualnym czasem
     if (!seeded) {
         std::srand(12345);
         seeded = true;
     }
+}
 
-    // przechodzimy po całej tablicy i wpisujemy do niej liczby losowe
-    for (int i = 0; i < array.getSize(); ++i) {
+// wspólna funkcja do wypełniania struktury losowymi liczbami
+template <typename Structure>
+static bool fillRandomImpl(Structure& structure) {
+    seedRandomOnce();
 
-        if (!array.set(i, std::rand())) {
-            return false;    // jeśli nie uda się zapisać wartości do tablicy, zwracamy false
-
+    // przechodzimy po całej strukturze i wpisujemy liczby losowe
+    for (int i = 0; i < structure.getSize(); ++i) {
+        if (!structure.set(i, std::rand())) {
+            return false;
         }
     }
+
     return true;
 }
 
-// tworzy kopię podanej tablicy
-Array* RandomArrayGenerator::copyArray(const Array& source) {
-    // tworzymy nową tablicę o takim samym rozmiarze jak source
-    Array* copy = new (std::nothrow) Array(source.getSize());
-    // std::nothrow - przy braku pamięci dostaniemy nullptr
+// wspólna funkcja do kopiowania list
+template <typename ListType>
+static ListType* copyList(const ListType& source) {
+    // tworzymy nową pustą listę
+    ListType* copy = new (std::nothrow) ListType();
 
-    // jeśli nie udało się utworzyć nowej tablicy, zwracamy nullptr
+    // jeśli nie udało się przydzielić pamięci, zwracamy nullptr
     if (copy == nullptr) {
         return nullptr;
     }
 
-    // przepisujemy elementy z tablicy source do nowej tablicy
+    // przepisujemy elementy z oryginalnej listy do nowej listy
     for (int i = 0; i < source.getSize(); ++i) {
-        int value = 0;   // value przechowuje odczytaną liczbę
+        int value = 0;
 
-
-        // odczytujemy element z oryginalnej tablicy
         if (!source.get(i, value)) {
             delete copy;
             return nullptr;
         }
 
-        // zapisujemy odczytaną wartość do nowej tablicy
+        if (!copy->pushBack(value)) {
+            delete copy;
+            return nullptr;
+        }
+    }
+
+    return copy;
+}
+
+// ===== fillRandom =====
+
+bool RandomArrayGenerator::fillRandom(Array& array) {
+    return fillRandomImpl(array);
+}
+
+bool RandomArrayGenerator::fillRandom(SingleList& list) {
+    return fillRandomImpl(list);
+}
+
+bool RandomArrayGenerator::fillRandom(DoubleList& list) {
+    return fillRandomImpl(list);
+}
+
+// ===== copy =====
+
+Array* RandomArrayGenerator::copyArray(const Array& source) {
+    // tworzymy nową tablicę o takim samym rozmiarze
+    Array* copy = new (std::nothrow) Array(source.getSize());
+
+    if (copy == nullptr) {
+        return nullptr;
+    }
+
+    // przepisujemy wszystkie wartości do nowej tablicy
+    for (int i = 0; i < source.getSize(); ++i) {
+        int value = 0;
+
+        if (!source.get(i, value)) {
+            delete copy;
+            return nullptr;
+        }
+
         if (!copy->set(i, value)) {
             delete copy;
             return nullptr;
         }
     }
 
-    // zwracamy gotową kopię tablicy
     return copy;
+}
+
+SingleList* RandomArrayGenerator::copySingleList(const SingleList& source) {
+    return copyList(source);
+}
+
+DoubleList* RandomArrayGenerator::copyDoubleList(const DoubleList& source) {
+    return copyList(source);
 }
