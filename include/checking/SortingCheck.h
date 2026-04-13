a4dbbd5c61e3271130f15cb06ac13fa0bf41f2b6
MashaGuzhva <mguzhvao.o@gmail.com>
2026-04-14 00:37:03 +0200

    sortingcheck

--- include/checking/SortingCheck.h
+++ include/checking/SortingCheck.h
@@ -4,22 +4,60 @@
 
 #ifndef PROJECT_SORTINGCHECK_H
 #define PROJECT_SORTINGCHECK_H
+
 #include "structures/Array.h"
 #include "structures/DoubleList.h"
 #include "structures/SingleList.h"
 
 class SortingCheck {
+private:
+    // template pozwala napisać jedną wspólną funkcję dla różnych typów struktur
+    // Structure będzie tutaj zastąpione prawdziwym typem, np Array<int> albo SingleList<double>
+    template <typename Structure, typename T>
+    static bool sortedAscendImpl(const Structure& structure) {
+        // const Structure& oznacza, że struktura jest przekazywana bez kopiowania
+        // const oznacza, że funkcja tylko sprawdza dane i nie może ich zmieniać
+
+        // zaczynamy od drugiego elementu
+        // wtedy możemy porównać go z poprzednim
+        for (int i = 1; i < structure.getSize(); ++i) {
+            T last{};      // poprzedni element
+            T current{};   // aktualny element
+            // {} oznacza domyślną inicjalizację zmiennej
+
+            // jeśli odczyt się nie uda, zwracamy false
+            if (!structure.get(i - 1, last) || !structure.get(i, current)) {
+                return false;
+            }
+
+            // jeśli aktualny element jest mniejszy od poprzedniego
+            // struktura nie jest posortowana rosnąco
+            if (current < last) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 public:
-     //static - metoda jest niezależna od obiektu
-     //nie ma potrzeby tworzenia obiektu QuickSort
-     //funkcja została po prostu umieszczona w klasie
-     // bool - funkcja zwraca true albo false
-     // Array& - referencja na oryginalne tablice
-     static bool SortedAscend(const Array<int>& array);
-     static bool SortedAscend(const DoubleList<int>& list);
-     static bool SortedAscend(const SingleList<int>& list);
+    // sprawdza sortowanie dla tablicy dowolnego typu T
+    template <typename T>
+    static bool SortedAscend(const Array<T>& array) {
+        return sortedAscendImpl<Array<T>, T>(array);
+    }
 
+    // sprawdza sortowanie dla listy jednokierunkowej dowolnego typu T
+    template <typename T>
+    static bool SortedAscend(const SingleList<T>& list) {
+        return sortedAscendImpl<SingleList<T>, T>(list);
+    }
+
+    // sprawdza sortowanie dla listy dwukierunkowej dowolnego typu T
+    template <typename T>
+    static bool SortedAscend(const DoubleList<T>& list) {
+        return sortedAscendImpl<DoubleList<T>, T>(list);
+    }
 };
-
 
 #endif //PROJECT_SORTINGCHECK_H
