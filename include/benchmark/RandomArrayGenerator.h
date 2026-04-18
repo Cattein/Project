15044e145395008ad082e40a78237c1fc279e20e
MashaGuzhva <mguzhvao.o@gmail.com>
2026-04-18 15:10:42 +0200

    change random seed - nullptr

--- include/benchmark/RandomArrayGenerator.h
+++ include/benchmark/RandomArrayGenerator.h
@@ -1,14 +1,10 @@
-//
-// Created by MashaGuzhva on 12/04/2026.
-//
-
 #ifndef PROJECT_RANDOMARRAYGENERATOR_H
 #define PROJECT_RANDOMARRAYGENERATOR_H
 
 #include <new>
 #include <cstdlib>
+#include <ctime>
 #include <string>
-#include <random>
 
 #include "structures/Array.h"
 #include "structures/SingleList.h"
@@ -18,14 +14,14 @@ class RandomArrayGenerator {
 private:
     // ustawia ziarno generatora tylko raz
     static void seedRandomOnce() {
-        // static - zmienna seeded zostanie utworzona tylko raz (przy kolejnych wywołaniach funkcji zachowa swoją wcześniejszą wartość)
-
+        // static - zmienna seeded zostanie utworzona tylko raz
+        // przy kolejnych wywołaniach funkcji zachowa swoją wcześniejszą wartość
         static bool seeded = false;
 
         if (!seeded) {
-            std::srand(12345);
-            // std::srand ustawia punkt startowy generatora std::rand
-            // 12345 to seed, czyli wartość początkowa
+            std::srand(static_cast<unsigned int>(std::time(nullptr)));
+            // std::time(nullptr) zwraca aktualny czas
+            // dzięki temu przy każdym uruchomieniu programu seed będzie inny
 
             seeded = true;
         }
@@ -34,8 +30,6 @@ private:
     // losuje wartość typu int
     static int randomInt() {
         // std::rand zwraca liczbę pseudolosową z zakresu od 0 do RAND_MAX
-        // RAND_MAX to stała z biblioteki C/C++, określa maksymalną wartość rand()
-
         // odejmujemy połowę RAND_MAX, żeby otrzymać także liczby ujemne
         return std::rand() - (RAND_MAX / 2);
     }
@@ -49,14 +43,14 @@ private:
         // dzięki temu dzielenie nie będzie całkowite
 
         // potem przesuwamy zakres do -1000000 .. 1000000
-        // 2000000 - szerokość całego przedziału
         return -1000000.0f + zeroToOne * 2000000.0f;
     }
 
     // losuje wartość typu double
     static double randomDouble() {
         // zamieniamy wynik rand na liczbę z zakresu 0..1
-        const double zeroToOne =static_cast<double>(std::rand()) / static_cast<double>(RAND_MAX);
+        const double zeroToOne =
+            static_cast<double>(std::rand()) / static_cast<double>(RAND_MAX);
         // static_cast<double> zamienia liczbę na typ double
 
         // potem przesuwamy zakres do -1000000 .. 1000000
@@ -66,7 +60,6 @@ private:
     // losuje wartość typu unsigned int
     static unsigned int randomUnsignedInt() {
         // static_cast<unsigned int> zamienia wynik na typ bez znaku
-        // liczby - dodatnie albo równe 0
         return static_cast<unsigned int>(std::rand());
     }
 
@@ -74,39 +67,20 @@ private:
     static std::string randomString() {
         const std::string characters =
             "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        // const std::string - po utworzeniu tego napisu
-        // nie można już zmienić jego zawartości
         // characters zawiera wszystkie znaki, z których będziemy budować losowy napis
 
-        // długość napisu od 1 do 20 znaków
         const int length = (std::rand() % 20) + 1;
-        // % 20 daje zakres 0..19
-        // +1 przesuwa ten zakres do 1..20
-
-        static std::random_device randomDevice;
-        // std::random_device służy do uzyskania wartości startowej
-        // dla generatora liczb losowych
-
-        static std::mt19937 generator(randomDevice());
-        // std::mt19937 to generator liczb pseudolosowych z biblioteki C++
-        // static - generator tworzymy tylko raz
-        // dzięki temu nie tworzymy nowego generatora przy każdym wywołaniu funkcji
-
-        std::uniform_int_distribution<> distribution(
-            0,
-            static_cast<int>(characters.size()) - 1
-        );
-        // distribution losuje indeks znaku
-        // 0 - pierwszy znak w napisie characters
-        // characters.size() - 1 - ostatni poprawny indeks
-        // static_cast<int> zamienia size_t na int
+        // długość napisu od 1 do 20 znaków
 
         std::string randomStringValue;
         // pusty napis, do którego będziemy dopisywać kolejne znaki
 
         for (int i = 0; i < length; ++i) {
-            randomStringValue += characters[distribution(generator)];
-            // losujemy indeks znaku i dopisujemy odpowiedni znak do napisu
+            const int index = std::rand() % static_cast<int>(characters.size());
+            // losujemy indeks znaku z napisu characters
+
+            randomStringValue += characters[index];
+            // dopisujemy wylosowany znak do napisu
         }
 
         return randomStringValue;
@@ -116,11 +90,9 @@ public:
     // ===== fillRandom dla Array =====
 
     static bool fillRandom(Array<int>& array) {
+        seedRandomOnce();
         // Array<int>& - tablica jest przekazywana przez referencję
         // funkcja działa na oryginalnej tablicy, a nie na kopii
-
-        seedRandomOnce();
-
         for (int i = 0; i < array.getSize(); ++i) {
             if (!array.set(i, randomInt())) {
                 return false;
@@ -132,7 +104,6 @@ public:
 
     static bool fillRandom(Array<float>& array) {
         seedRandomOnce();
-
         for (int i = 0; i < array.getSize(); ++i) {
             if (!array.set(i, randomFloat())) {
                 return false;
@@ -144,7 +115,6 @@ public:
 
     static bool fillRandom(Array<double>& array) {
         seedRandomOnce();
-
         for (int i = 0; i < array.getSize(); ++i) {
             if (!array.set(i, randomDouble())) {
                 return false;
@@ -156,7 +126,6 @@ public:
 
     static bool fillRandom(Array<unsigned int>& array) {
         seedRandomOnce();
-
         for (int i = 0; i < array.getSize(); ++i) {
             if (!array.set(i, randomUnsignedInt())) {
                 return false;
@@ -168,7 +137,6 @@ public:
 
     static bool fillRandom(Array<std::string>& array) {
         seedRandomOnce();
-
         for (int i = 0; i < array.getSize(); ++i) {
             if (!array.set(i, randomString())) {
                 return false;
@@ -181,10 +149,9 @@ public:
     // ===== fillRandom dla SingleList =====
 
     static bool fillRandom(SingleList<int>& list) {
+        seedRandomOnce();
         // SingleList<int>& - lista jest przekazywana przez referencję
         // funkcja wpisuje dane bezpośrednio do prawdziwej listy
-
-        seedRandomOnce();
 
         for (int i = 0; i < list.getSize(); ++i) {
             if (!list.set(i, randomInt())) {
@@ -197,7 +164,6 @@ public:
 
     static bool fillRandom(SingleList<float>& list) {
         seedRandomOnce();
-
         for (int i = 0; i < list.getSize(); ++i) {
             if (!list.set(i, randomFloat())) {
                 return false;
@@ -209,7 +175,6 @@ public:
 
     static bool fillRandom(SingleList<double>& list) {
         seedRandomOnce();
-
         for (int i = 0; i < list.getSize(); ++i) {
             if (!list.set(i, randomDouble())) {
                 return false;
@@ -221,7 +186,6 @@ public:
 
     static bool fillRandom(SingleList<unsigned int>& list) {
         seedRandomOnce();
-
         for (int i = 0; i < list.getSize(); ++i) {
             if (!list.set(i, randomUnsignedInt())) {
                 return false;
@@ -233,7 +197,6 @@ public:
 
     static bool fillRandom(SingleList<std::string>& list) {
         seedRandomOnce();
-
         for (int i = 0; i < list.getSize(); ++i) {
             if (!list.set(i, randomString())) {
                 return false;
@@ -246,10 +209,9 @@ public:
     // ===== fillRandom dla DoubleList =====
 
     static bool fillRandom(DoubleList<int>& list) {
+        seedRandomOnce();
         // DoubleList<int>& - lista dwukierunkowa jest przekazywana przez referencję
         // funkcja zmienia prawdziwą strukturę danych
-
-        seedRandomOnce();
 
         for (int i = 0; i < list.getSize(); ++i) {
             if (!list.set(i, randomInt())) {
@@ -262,7 +224,6 @@ public:
 
     static bool fillRandom(DoubleList<float>& list) {
         seedRandomOnce();
-
         for (int i = 0; i < list.getSize(); ++i) {
             if (!list.set(i, randomFloat())) {
                 return false;
@@ -274,7 +235,6 @@ public:
 
     static bool fillRandom(DoubleList<double>& list) {
         seedRandomOnce();
-
         for (int i = 0; i < list.getSize(); ++i) {
             if (!list.set(i, randomDouble())) {
                 return false;
@@ -286,7 +246,6 @@ public:
 
     static bool fillRandom(DoubleList<unsigned int>& list) {
         seedRandomOnce();
-
         for (int i = 0; i < list.getSize(); ++i) {
             if (!list.set(i, randomUnsignedInt())) {
                 return false;
@@ -298,7 +257,6 @@ public:
 
     static bool fillRandom(DoubleList<std::string>& list) {
         seedRandomOnce();
-
         for (int i = 0; i < list.getSize(); ++i) {
             if (!list.set(i, randomString())) {
                 return false;
